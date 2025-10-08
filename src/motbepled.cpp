3973b763ca8844618aadc10cd8e0e41f6f836aa4
izyino <info@izyino.com.br>
2025-10-08 00:08:51 -0300

    Update motbepled.cpp

--- src/motbepled.cpp
+++ src/motbepled.cpp
@@ -85,7 +85,7 @@ void motbepled::begin() {
   }
   if (pinoBeep>=0){
     pinMode(pinoBeep, OUTPUT);                       //beep
-    ledcAttachPin(pinoBeep, 4);                      //define pinoLed channel 5 (beep)
+    ledcAttachPin(pinoBeep, 4);                      //define pinoLed channel 4 (beep)
     ledcSetup(4, 1000, 8);                           //PWM sempre a 1KHz
     ledcWrite(4, 0);                                 //grava 0 nele (silencia)
   }
@@ -376,6 +376,7 @@ void  motbepled::writ(uint8_t px1, uint8_t px2, uint8_t px3, uint8_t px4)
  digitalWrite(pinosStep[k][0],px1);digitalWrite(pinosStep[k][1],px2);digitalWrite(pinosStep[k][2],px3);digitalWrite(pinosStep[k][3],px4);
 }
 //----------------------------------------------------------------------
+
 
 
 
